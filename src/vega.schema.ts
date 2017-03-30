da14afebcb0aaaea47f270e7de7141e59c52b433
Will Strimling <w@ml.to>
2017-03-30 01:51:13 -0700

    add VgProjection to schema

--- src/vega.schema.ts
+++ src/vega.schema.ts
@@ -89,6 +89,45 @@ export type VgScale = {
   zero?: boolean
 };
 
+export type VgProjection = {
+  /*
+   * The type of the projection.
+   */
+  type?: string;
+  /*
+   * The center of the projection.
+   */
+  center?: number[];
+  /*
+   * The translation of the projection.
+   */
+  translate?: number[];
+  /*
+   * The scale of the projection.
+   */
+  zoom?: number;
+  /**
+   * The rotation of the projection.
+   */
+  rotate?: number[];
+  /*
+   * The desired precision of the projection.
+   */
+  precision?: String;
+  /*
+   * The clip angle of the projection.
+   */
+  clipAngle?: number;
+  /*
+   * Sets the projection’s scale factor to the specified value
+   */
+  scale?: number;
+  /*
+   * Sets the projection’s viewport clip extent to the specified bounds in pixels
+   */
+  clipExtent?: number[];
+};
+
 export function isDataRefUnionedDomain(domain: VgDomain): domain is DataRefUnionDomain {
   if (!isArray(domain)) {
     return 'fields' in domain && !('data' in domain);
